6b2ae8640335b0ac9987416380fde3869eaa3244
prestonb25 <prestonb25@nycstudents.net>
2026-09-16 06:19:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,52 @@ t.shape('turtle')
     t.left(90)
 square(200) """
 
-turtle.done()
+
+""" def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) 
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+
+ """
+
+
+""" def rectangle (x):
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+rectangle (200)
+turtle.done() """
+
+
+""" def equal(x):
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+equal(200) 
+turtle.done() """
+
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+triangle(100,90)
